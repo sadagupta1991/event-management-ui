f58b8c463290526e357f19053d7cd06112da00bf
sadagupta1991 <sada.gupta1991@gmail.com>
2025-11-14 00:56:18 +0530

    add

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   base: '/event-management-ui/', // Set the base path for GitHub Pages deployment
-  plugins: [react({
-    jsxRuntime: 'classic'
-  })],
+  plugins: [react()],
   server: {
     port: 3000,
   },
